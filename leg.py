4c284d765add377bdc6751ea1a3daf8090a64bc9
Каштанов Александр Сергеевич <aleks-hd2015@yandex.ru>
2022-11-29 21:57:38 +0400

    lesson 2

--- leg.py
+++ leg.py
@@ -1,43 +1,55 @@
-# 1) Напишите программу, которая принимает на вход цифру, 
-# обозначающую день недели, и проверяет, является ли этот день выходным.
+# 1) Напишите программу, которая принимает на вход число N и 
+# выдает набор произведений чисел от 1 до N.
+# Пример:
+# пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
-numberDay = abs(int(input("Введите день недели: ")))
+n = int(input("Введите число = : "))
+array = []
+def getMultiply(n):
+    multiply = 1    
+    for i in range(1, n+1):
+        multiply *= i
+        array.append(multiply)  
 
-def checkDayOfWeek(numberDay):
-    if (numberDay > 0 and numberDay < 6): print("Нет")
-    if (numberDay > 6 and numberDay < 8): print("Да")
-    else: print("Не правильно указанное число")
+getMultiply(n)
+print(array) 
 
-checkDayOfWeek(numberDay)
-print("\n")
+#2) Требуется найти наименьший натуральный делитель
+#  целого числа N, отличный от 1.
+  
+n = int(input("Введите число = : "))
+def getMinDel(n):
+    k = 2
+    flag = False
+    while (flag == 0):
+        if (n % k == 0):
+            flag = True
+            print(k)
+        else:
+            k +=1
+getMinDel(n) 
 
-# 2) Напишите программу, которая принимает на вход координаты точки (X и Y),
-#  причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости,
-#в которой находится эта точка (или на какой оси она находится).
+#3) Задайте список из (2*N+1) элементов, заполненных числами из 
+# промежутка [-N, N]. Найдите произведение элементов на указанных ИНДЕКСАХ.
+# Пять ИНДЕКСОВ хранятся в списке, который вы сами заполняете.
+# Пример списка ИНДЕКСОВ [2, 2, 3, 1, 8]
+# Ввод: 4
+# [-4, -3, -2, -1, 0, 1, 2, 3,4]
 
-x = int(input("Введите X = : "))
-y = int(input("Введите Y = : "))
+n = abs(int(input("Введите число = : ")))
+a = []
+def getArray(n):
+    m = n * -1
+    for m in range(m, n+1):
+        a.append(m)
+        a.reverse
+getArray(n)
+print(a)
 
-def getNumberPlateQuater(x, y):
-    if (x > 0 and y > 0): print(1)
-    if (x < 0 and y > 0): print(2)
-    if (x < 0 and y < 0): print(3)
-    else: print(4)
-
-getNumberPlateQuater(x, y)
-
-# 3) Напишите программу, которая принимает на вход координаты двух 
-# точек и находит расстояние между ними в 2D пространстве.
-
-import math
-
-x0 = int(input("Введите X0 = : "))
-y0 = int(input("Введите Y0 = : "))
-
-x1 = int(input("Введите X1 = : "))
-y1 = int(input("Введите Y1 = : "))
-
-def getDistance(x0,y0, x1, y1):
-    return math.sqrt( ((x1 - x0)**2) + ((y1 - y0)**2))
-
-print(getDistance(x0, y0, x1, y1))    
+randomArray = [2, 2, 3, 1, 8]
+def findMultiply(array: list):
+    k = 1
+    for i in range(0, len(array)):
+        k *=array[i]
+    return k
+print("Произведение элементов произвольного списка =", findMultiply(randomArray))
